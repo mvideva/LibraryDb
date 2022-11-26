d65ff545bbb6d8e857bbf5fa4f763f4182ba4dc0
magdavideva <MMV33@pitt.edu>
2022-11-26 17:06:37 -0500

    Completed Check In and Check Out. Error with updating

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryApp.Models
+{
+    public class CustomerModel
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
+
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using LibraryApp.Models;
+﻿using System.Diagnostics;
+using LibraryApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MySql.Data.MySqlClient;
@@ -8,60 +9,134 @@ namespace LibraryDbApp.Pages;
 public class IndexModel : PageModel
 {
     public IList<BookModel> Books { get; set; } = default!;
+    public IList<CustomerModel> Customers { get; set; } = default!;
 
     [BindProperty(SupportsGet = true)]
-    public string? SearchTitle { get; set; }
+    public string SearchTitle { get; set; } = default!;
 
     [BindProperty(SupportsGet = true)]
-    public string? SearchAuthor { get; set; }
+    public string SearchAuthor { get; set; } = default!;
+
+    [BindProperty(SupportsGet = true)]
+    public string BookAction { get; set; } = default!;
+
+    [BindProperty(SupportsGet = true)]
+    public string BookIdCheckedOut { get; set; } = default!;
+
+    [BindProperty(SupportsGet = true)]
+    public string BookCheckedOutBy { get; set; } = default!;
 
     public void OnGet()
     {
         // As described in https://dev.mysql.com/doc/connector-net/en/connector-net-tutorials-sql-command.html
         var connStr = "server=localhost;port=3306;database=LibraryDb;user=lib;password=rary";
         var conn = new MySqlConnection(connStr);
-        conn.Open();
+        MySqlDataReader? rdr = null;
 
-        var sql = @"
-        SELECT books.id, books.title, books.author, books.genre, customers.name, checkouts.due_date
-        FROM((books
-        INNER JOIN checkouts ON checkouts.book_id = books.id)
-        INNER JOIN customers ON checkouts.customer_id = customers.id)";
-
-        if(SearchTitle != null || SearchAuthor != null)
+        try
         {
-            string where;
-            if(SearchTitle != null && SearchAuthor != null)
+            conn.Open();
+            var sql = @"
+        SELECT books.id, books.title, books.author, books.genre, customers.name, customers.id, checkouts.due_date
+        FROM((books
+        LEFT JOIN checkouts ON checkouts.book_id = books.id)
+        LEFT JOIN customers ON checkouts.customer_id = customers.id)";
+
+            if (SearchTitle != null || SearchAuthor != null)
             {
-                where = $" WHERE books.title LIKE '%{SearchTitle}%' and books.author LIKE '%{SearchAuthor}%'"; 
+                string where;
+                if (SearchTitle != null && SearchAuthor != null)
+                {
+                    where = $" WHERE books.title LIKE '%{SearchTitle}%' AND books.author LIKE '%{SearchAuthor}%'";
+                }
+                else if (SearchTitle != null)
+                {
+                    where = $" WHERE books.title LIKE '%{SearchTitle}%'";
+                }
+                else
+                {
+                    where = $" WHERE books.author LIKE '%{SearchAuthor}%'";
+                }
+                sql += where;
             }
-            else if(SearchTitle != null)
+
+            var cmd = new MySqlCommand(sql, conn);
+            rdr = cmd.ExecuteReader();
+
+            Books = new List<BookModel>();
+
+            while (rdr.Read())
             {
-                where = $" WHERE books.title LIKE '%{SearchTitle}%'";
+                Books.Add(new BookModel()
+                {
+                    Id = rdr[0].ToString(),
+                    Title = rdr[1].ToString(),
+                    Author = rdr[2].ToString(),
+                    Genre = rdr[3].ToString(),
+                    CheckedOutBy = rdr[4].ToString(),
+                    CheckedOutUntil = rdr[5].ToString()
+                });
+            }
+
+            rdr.Close();
+            sql = "SELECT id, name FROM customers ORDER BY name";
+            cmd = new MySqlCommand(sql, conn);
+            rdr = cmd.ExecuteReader();
+            
+            Customers = new List<CustomerModel>();
+
+            while (rdr.Read())
+            {
+                Customers.Add(new CustomerModel()
+                {
+                    Id = rdr[0].ToString(),
+                    Name = rdr[1].ToString(),
+                });
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Print($"An error occured ({e.Message})");
+        }
+        finally
+        {
+            
+            rdr?.Close();
+            conn.Close();
+        }
+    }
+
+    public void OnPost()
+    {
+        var connStr = "server=localhost;port=3306;database=LibraryDb;user=lib;password=rary";
+        var conn = new MySqlConnection(connStr);
+
+        try
+        {
+            conn.Open();
+            string sql;
+            var isCheckedOut = BookAction.Equals("Check Out");
+            if(isCheckedOut)
+            {
+                var dueDate = DateTime.Now.AddDays(14).ToString("yyyy-MM-dd HH:mm:ss.fff"); 
+                sql = $"INSERT INTO checkouts (customer_id, book_id, due_date) VALUES ({BookCheckedOutBy},'{BookIdCheckedOut}','{dueDate}')";
             }
             else
             {
-                where = $" WHERE books.author LIKE '%{SearchAuthor}%'";
+                sql = $"UPDATE checkouts SET return_date = '{DateTime.Now.ToString()}' WHERE book_id = '{BookIdCheckedOut}' AND return_date IS NULL";
             }
-            sql += where;
+
+            var cmd = new MySqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
         }
-
-        var cmd = new MySqlCommand(sql, conn);
-        var rdr = cmd.ExecuteReader();
-
-        Books = new List<BookModel>();
-
-        while (rdr.Read())
+        catch (Exception e)
         {
-            Books.Add(new BookModel()
-            {
-                Id = rdr[0].ToString(),
-                Title = rdr[1].ToString(),
-                Author = rdr[2].ToString(),
-                Genre = rdr[3].ToString(),
-                CheckedOutBy = rdr[4].ToString(),
-                CheckedOutUntil = rdr[5].ToString()
-            });
+            Debug.Print($"An error occured ({e.Message})");
+        }
+        finally
+        {
+            conn.Close();
+            RedirectToAction("Get");
         }
     }
 }
