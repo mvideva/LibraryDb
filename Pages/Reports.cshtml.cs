fad8bc64a50c22312739536c2b92a034fffb7a65
Magda Videva <mmv33@pitt.edu>
2022-11-30 19:44:37 -0500

    Added Reports Pages

---
+++ Pages/Reports.cshtml.cs
@@ -0,0 +1,18 @@
+using LibraryApp.Models;
+using LibraryDbApp.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryDbApp.Pages
+{
+    public class ReportsModel : PageModel
+    {
+        public IList<StaffModel> Staff { get; set; } = default!;
+        public IList<CustomerModel> Customers { get; set; } = default!;
+
+        public void OnGet()
+        {
+            Staff = DbService.GetStaffReport();
+            Customers = DbService.GetCustomerReport();
+        }
+    }
+}
